b82930cecf268bbdcbc6e9b01cf8a65ddcf98f49
JoshStead5674 <157617840+JoshStead5674@users.noreply.github.com>
2024-01-24 10:49:57 -0500

    Update Joshua.stead_8ball.py
    first stopping point

--- Joshua.stead_8ball.py
+++ Joshua.stead_8ball.py
@@ -1,1 +1,10 @@
+import random
 
+fortunes = ["Yes", "No", "Probably not", "Certaintly", "I guess so", "It's likely", "Not happening"]
+
+print("0) See a random fortune")
+print("1) List all possible fortunes")
+print("2) Get a fortune")
+userChoice = input("What do you want to do: ")
+if userChoice == "0":
+    print("Hey this is working so far")
